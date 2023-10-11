cbfd7c0bd9fa58cb5423155aa23d9e645f4e6f63
Jenya <113167825+Genya10@users.noreply.github.com>
2023-10-11 14:45:26 +0300

    add app about clients

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { addClientAction,removeClientAction } from './store/customerReducer';
+import { addCashAction,getAddAction } from './store/cashReducer';
+import { fetchCustomers } from './asincAction/customers';
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cash.cash);
+  const customers = useSelector(state=>state.customers.customers);
+
+  const addCash = (cash) => {
+   dispatch(addCashAction(cash));
+  };
+  const getCash = (cash) => {
+   dispatch(getAddAction(cash));
+  };
+  const addClient = (name) => {
+    const client ={
+      name,
+      id:Date.now(),
+    }
+    dispatch(addClientAction(client));
+   };
+   const deleteClient = (client) => {
+     dispatch(removeClientAction(client.id));
+   };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h2 style={{fontSize:'3rem'}}>Balance:{cash}</h2>
+     <div className='div-btn'>
+      <button className='btn' onClick={()=>addCash(Number(prompt()))}>Add cash</button>
+      <button className='btn' onClick={()=>getCash(Number(prompt()))}>Get cash</button>
+      <button className='btn' onClick={()=>addClient(prompt())}>Add client</button>
+      <button className='btn' onClick={()=>dispatch(fetchCustomers())}>Get client</button>
+     </div>
+     {customers.length > 0     
+      ? <div>
+       {customers.map(client=>
+        <div key={client.id} onClick={()=>{deleteClient(client)}}
+         className='customers'
+        >{client.name}</div>
+       )}
+       </div>   
+      : <div>
+         <h3>NO CLIENTS</h3></div>}
+       </div>
   );
 }
 
